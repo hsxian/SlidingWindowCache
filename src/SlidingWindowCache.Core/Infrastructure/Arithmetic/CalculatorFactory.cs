5ed1e166a46ca49e61df7260ced77c30e8a4e78a
hsxian <940100027@qq.com>
2019-08-26 20:29:10 +0800

    cache key and test

--- src/SlidingWindowCache.Core/Infrastructure/Arithmetic/CalculatorFactory.cs
+++ src/SlidingWindowCache.Core/Infrastructure/Arithmetic/CalculatorFactory.cs
@@ -2,15 +2,15 @@ using System;
 
 namespace SlidingWindowCache.Core.Infrastructure.Arithmetic
 {
-    public class CalculatorFactory
+    public static class CalculatorFactory
     {
-        public static ICalculator<T>  Build<T>()
+        public static ICalculator<T> Build<T>()
         {
-            if(typeof(T)==typeof(int))return (ICalculator<T>) new Int32Calculator();
-            if(typeof(T)==typeof(long))return (ICalculator<T>) new Int64Calculator();
-            if(typeof(T)==typeof(double))return (ICalculator<T>) new DoubleCalculator();
+            if (typeof(T) == typeof(int)) return (ICalculator<T>) new Int32Calculator();
+            if (typeof(T) == typeof(long)) return (ICalculator<T>) new Int64Calculator();
+            if (typeof(T) == typeof(float)) return (ICalculator<T>) new SingleCalculator();
+            if (typeof(T) == typeof(double)) return (ICalculator<T>) new DoubleCalculator();
             throw new NotSupportedException();
         }
-
     }
 }
